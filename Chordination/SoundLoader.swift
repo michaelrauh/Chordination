9e340382e2771bc25baa0c953b4404cc594281c0
Linus Falck-Ytter <lifayt@gmail.com>
2016-04-03 18:25:02 -0400

    Added data structure that holds all the audio players

--- Chordination/SoundLoader.swift
+++ Chordination/SoundLoader.swift
@@ -13,13 +13,25 @@ class SoundLoader {
     
     var sound = NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("../sounds/45", ofType: "wav")!)
     var audioPlayer = AVAudioPlayer()
+    var sounds = [Int: AVAudioPlayer]()
     
     init () {
         audioPlayer = try! AVAudioPlayer(contentsOfURL: sound)
         audioPlayer.prepareToPlay()
+        load()
+    }
+    
+    func load() {
+        for index in 23...80 {
+            let urlStub = String(index)
+            let note = NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("../sounds/\(urlStub)", ofType: "wav")!)
+            sounds[index] = try! AVAudioPlayer(contentsOfURL: note)
+        }
     }
     
     func play() {
-        audioPlayer.play()
+        sounds[66]!.play()
     }
+    
+    
 }
